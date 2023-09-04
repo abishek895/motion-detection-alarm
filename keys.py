7717123cdbfbc7ecb5791adfea6f8aaaf46be5d4
abishek895 <132159982+abishek895@users.noreply.github.com>
2023-09-04 19:02:03 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,4 @@
+account_sid = 'AC05a2cb8d3956c24d588549e982f3db20'
+auth_token = 'b8187224ebcceffa52d18e134e7b6bb4'
+twilio_number = '+13203734058'
+owner_number = 'Enter Your Number Here To Get Message'
